c8218c58d2efa6abe0b7d9c50ac7a6c2ef1839b6
GabrielJS2005 <gabriel.jose2005@hotmail.com>
2026-08-26 16:55:25 -0300

    Alterando MainActivity para criar a TarefaViewModel e iniciar a aplicação utilizando AppNavigation

--- app/src/main/java/GabrielJS2005/com/github/toDoList/MainActivity.kt
+++ app/src/main/java/GabrielJS2005/com/github/toDoList/MainActivity.kt
@@ -1,17 +1,13 @@
 package GabrielJS2005.com.github.toDoList
 
+import GabrielJS2005.com.github.toDoList.navigation.AppNavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import GabrielJS2005.com.github.toDoList.ui.theme.FiaptodolistTheme
+import GabrielJS2005.com.github.toDoList.viewmodel.TarefaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +15,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FiaptodolistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FiaptodolistTheme {
-        Greeting("Android")
     }
 }
